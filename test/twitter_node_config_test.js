014eaf3db3e4738e6602a04efea6908a0bff6354
rick <technoweenie@gmail.com>
2010-01-27 15:42:15 -0800

    add test example with newline inside the json body

--- test/twitter_node_config_test.js
+++ test/twitter_node_config_test.js
@@ -20,8 +20,8 @@ describe("json TwitterNode instance")
     this.twit.processTweet(" ")
     this.twit.processTweet("{")
     this.twit.processTweet('"a":{')
-    this.twit.processTweet('"b":1}')
-    this.twit.processTweet("}\n{\"a\":1}")
+    this.twit.processTweet('"b":1')
+    this.twit.processTweet("}\n}\n{\"a\":1}")
 
     if(!promise.hasFired && promise._blocking) promise.wait()
 
