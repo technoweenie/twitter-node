41f7a0f250905ad4c172c2b8083284106f63fdcd
Justin Page <xjustinpagex@gmail.com>
2015-01-08 16:29:30 -0800

    update: added examples/stream.js

---
+++ examples/stream.js
@@ -0,0 +1,18 @@
+var Twitter = require('../lib/twitter');
+
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
+/**
+ * Stream statuses filtered by keyword
+ * number of tweets per second depends on topic popularity
+ **/
+client.stream('statuses/filter', {track: 'nodejs'},  function(stream){
+	stream.on('data', function(tweet) {
+		console.log(tweet.text);
+	});
+});
