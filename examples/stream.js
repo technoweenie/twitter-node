557f0561ae38797379b8e960e3ffbd096e2cce95
Desmond Morris <hi@desmondmorris.com>
2015-01-24 13:24:49 -0500

    Remove geo helper methods and adds implementation examples

--- examples/stream.js
+++ examples/stream.js
@@ -1,10 +1,10 @@
 var Twitter = require('../lib/twitter');
 
 var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 /**
