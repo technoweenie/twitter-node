8ee7a95344aa672c2f82d67cde099556055fe2b6
Desmond Morris <hi@desmondmorris.com>
2015-01-25 11:31:07 -0500

    Adds support for preset endpoint base paths and completes test coverage for the constructor

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
-Twitter for Node.js  [![NPM](https://nodei.co/npm/twitter.png?mini=true)](https://nodei.co/npm/twitter/) [![wercker status](https://app.wercker.com/status/624dbe8ad011852d1e01d7dc03941fc5/s/master "wercker status")](https://app.wercker.com/project/bykey/624dbe8ad011852d1e01d7dc03941fc5)
-======================================
+# Twitter for Node.js
 
-[node-twitter](https://github.com/desmondmorris/node-twitter) aims to provide a complete, asynchronous client library for the Twitter API, including the REST, search and streaming endpoints. It's a fork of [@jdub](https://github.com/jdub)'s [twitter-node](https://github.com/jdub) which was inspired by, and used some code from, [@technoweenie](https://github.com/technoweenie)'s [twitter-node](https://github.com/technoweenie/twitter-node).
+An asynchronous client library for the Twitter [REST](https://dev.twitter.com/rest/public) and [Streaming](https://dev.twitter.com/streaming/overview) API's.
+
+[![wercker status](https://app.wercker.com/status/624dbe8ad011852d1e01d7dc03941fc5/s/master "wercker status")](https://app.wercker.com/project/bykey/624dbe8ad011852d1e01d7dc03941fc5) [![NPM](https://nodei.co/npm/twitter.png?mini=true)](https://nodei.co/npm/twitter/)
+
+```javascript
+var Twitter = require('twitter');
+
+var t = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+var params = {screen_name: 'nodejs'};
+t.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+```
 
 ## Installation
 
 `npm install twitter`
 
-## Configuration
+## Quick Start
 
 You will need valid Twitter developer credentials in the form of a set of consumer and access tokens/keys.  You can get these [here](https://apps.twitter.com/).  Do not forgot to adjust your permissions - most POST request require write permissions.
 
-````
+```javascript
 var Twitter = require('twitter');
 
 var client = new Twitter({
@@ -20,69 +39,86 @@ var client = new Twitter({
   access_token_key: '',
   access_token_secret: ''
 });
-````
+```
 
 Add your credentials accordingly.  I would use environment variables to keep your private info safe.  So something like:
 
-````
+```javascript
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
-````
+```
 
-## Usage
+You now have the ability to make GET and POST requests against the API via the convenience methods.
 
-The library comes with two helper methods for `get` and `post` requests.  To use, you simply need to pass the API path and parameters.  Example, lets get a list of favorites:
+```javascript
+client.get(path, params, callback);
+client.post(path, params, callback);
+client.stream(path, params, callback);
+```
 
-````
-// @see configuration above for the client variable
+## REST API
 
-client.get('favorites/list', function(error, params, response){
+You simply need to pass the endpoint and parameters to one of convenience methods.  Take a look at the [documentation site](https://dev.twitter.com/rest/public) to reference available endpoints.
+
+Example, lets get a [list of favorites](https://dev.twitter.com/rest/reference/get/favorites/list):
+
+```javascript
+client.get('favorites/list', function(error, tweets, response){
 
   if(error) throw error;
 
-  console.log(params);  // The favorites.
+  console.log(tweets);  // The favorites.
 
   console.log(response);  // Raw response object.
 
 });
+```
 
-````
+How about an example that passes parameters?  Let's  [tweet something](https://dev.twitter.com/rest/reference/post/statuses/update):
 
-How about an example that passes parameters?  Let's post a tweet:
-
-````
-// @see configuration above for the client variable
-
-client.post('statuses/update', {status: 'TYBG for twitter'},  function(error, params, response){
+```javascript
+client.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response){
 
   if(error) throw error;
 
-  console.log(params);  // Tweet body.
+  console.log(tweet);  // Tweet body.
 
   console.log(response);  // Raw response object.
 
 });
-````
+```
 
-Or even streaming? Let's see whose talking about javascript:
+## Streaming API
 
-````
-// @see configuration above for the client variable
+Using the `stream` convenience method, you to open and manipulate data via a stream piped directly from one of the streaming API's. Let's see who is talking about javascript:
 
+```javascript
 client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
 
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
 
-});
-````
+  stream.on('error', function(error) {
+    throw error;
+  });
 
-## [1.x Changes](https://github.com/desmondmorris/node-twitter/wiki/1.x)
+});
+```
+
+## Contributors
+
+Originally authored by  [@technoweenie](http://github.com/technoweenie)
+ and maintained by [@jdub](http://github.com/jdub)
+
+Currently maintained by [@desmondmorris](http://github.com/desmondmorris)
+
+[And we cannot forget the community](https://github.com/desmondmorris/node-twitter/graphs/contributors)
+
 
 ## LICENSE
 
--- test/twitter.js
+++ test/twitter.js
@@ -1,63 +1,179 @@
 'use strict';
 
 var assert = require('assert');
+var nock = require('nock');
 var Twitter = require('../lib/twitter');
+var VERSION = require('../package.json').version;
 
-describe("Constructor", function() {
+describe('Twitter', function() {
 
-  describe(".__generateURL()", function() {
-    var client;
+  describe('Constructor', function() {
 
-    before(function(){
-      client = new Twitter({});
-    });
+    describe('new Twitter();', function() {
 
-    it("method exists", function(){
-      assert(client.hasOwnProperty('__generateURL'));
-      assert.equal(typeof client.__generateURL, 'function');
-    });
+      var defaults = {};
 
-    it("build url", function(){
+      before(function(){
+        defaults = {
+          consumer_key: null,
+          consumer_secret: null,
+          access_token_key: null,
+          access_token_secret: null,
+          rest_base: 'https://api.twitter.com/1.1',
+          stream_base: 'https://stream.twitter.com/1.1',
+          user_stream_base: 'https://userstream.twitter.com/1.1',
+          site_stream_base: 'https://sitestream.twitter.com/1.1',
+          media_base: 'https://upload.twitter.com/1.1',
+          request_options: {
+            headers: {
+              'Accept': '*/*',
+              'Connection': 'close',
+              'User-Agent': 'node-twitter/' + VERSION
+            }
+          }
+        };
+      });
 
-      var path = 'statuses';
-      var endpoint = 'https://stream.twitter.com/1.1/statuses';
+      it('create new instance', function(){
+        var client = new Twitter();
+        assert(client instanceof Twitter);
+      });
 
-      assert.throws(
-        client.__generateURL,
-        Error
-      );
+      it('has default options', function(){
+        var client = new Twitter();
+        assert.equal(
+          Object.keys(defaults).length,
+          Object.keys(client.options).length
+        );
+        assert.deepEqual(
+          Object.keys(defaults),
+          Object.keys(client.options)
+        );
+      });
 
-      assert.equal(
-        client.__generateURL(path),
-        client.options.rest_base + '/' + path + '.json'
-      );
+      it('accepts and overrides options', function(){
+        var options = {
+          consumer_key: 'XXXXX',
+          power: 'Max',
+          request_options: {
+            headers: {
+              'Accept': 'application/json'
+            }
+          }
+        };
 
-      assert.equal(
-        client.__generateURL(path + '.json'),
-        client.options.rest_base + '/' + path + '.json'
-      );
+        var client = new Twitter(options);
 
-      assert.equal(
-        client.__generateURL('/' + path),
-        client.options.rest_base + '/' + path + '.json'
-      );
+        assert(client.options.hasOwnProperty('power'));
+        assert.equal(client.options.power, options.power);
 
-      assert.equal(
-        client.__generateURL(path + '/'),
-        client.options.rest_base + '/' + path + '.json'
-      );
+        assert.equal(client.options.consumer_key, options.consumer_key);
 
-      assert.equal(
-        client.__generateURL(endpoint),
-        endpoint + '.json'
-      );
+        assert.equal(
+          client.options.request_options.headers.Accept,
+          options.request_options.headers.Accept);
+      });
 
-      assert.equal(
-        client.__generateURL(endpoint),
-        endpoint + '.json'
-      );
+      it('has pre-configured request object', function(next){
+        var client = new Twitter({
+          request_options: {
+            headers: {
+              foo: 'bar'
+            }
+          }
+        });
+
+        assert(client.hasOwnProperty('request'));
+
+        nock('http://node.twitter').get('/').reply(200);
+        client.request.get('http://node.twitter/', function(error, response){
+
+          var headers = response.request.headers;
+
+          assert(headers.hasOwnProperty('foo'));
+          assert(headers.foo, 'bar');
+
+          assert.equal(headers['User-Agent'], 'node-twitter/' + VERSION);
+          assert(headers.hasOwnProperty('Authorization'));
+          assert(headers.Authorization.match(/^OAuth/));
+
+          next();
+        });
 
 
+      });
     });
   });
+
+  describe('Prototypes', function() {
+    describe('prototype.__buildEndpoint();', function() {
+      var client;
+
+      before(function(){
+        client = new Twitter({});
+      });
+
+      it('method exists', function(){
+        assert.equal(typeof client.__buildEndpoint, 'function');
+      });
+
+      it('build url', function(){
+        var path = 'statuses';
+        var endpoint = 'https://stream.twitter.com/1.1/statuses';
+
+        assert.throws(
+          client.__buildEndpoint,
+          Error
+        );
+
+        assert.equal(
+          client.__buildEndpoint(path),
+          client.options.rest_base + '/' + path + '.json'
+        );
+
+        assert.equal(
+          client.__buildEndpoint(path + '.json'),
+          client.options.rest_base + '/' + path + '.json'
+        );
+
+        assert.equal(
+          client.__buildEndpoint('/' + path),
+          client.options.rest_base + '/' + path + '.json'
+        );
+
+        assert.equal(
+          client.__buildEndpoint(path + '/'),
+          client.options.rest_base + '/' + path + '.json'
+        );
+
+        assert.equal(
+          client.__buildEndpoint(path, 'media'),
+          client.options.media_base + '/' + path + '.json'
+        );
+
+        assert.equal(
+          client.__buildEndpoint(endpoint),
+          endpoint + '.json'
+        );
+
+        assert.equal(
+          client.__buildEndpoint(endpoint),
+          endpoint + '.json'
+        );
+      });
+    });
+
+    describe('prototype.__request();', function() {
+    });
+
+    describe('prototype.__get();', function() {
+    });
+
+    describe('prototype.__post();', function() {
+    });
+
+    describe('prototype.__stream();', function() {
+    });
+  });
+
 });
